ed090def9b85d29f84d44134e70a711e1cde8655
Warren Paschetto <wdpaschetto@gmail.com>
2024-09-22 12:18:12 -0400

    Added new App screenshot

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,9 @@
 import { Button } from "@/components/ui/button";
-import {
-  BrainCogIcon,
-  EyeIcon,
-  GlobeIcon,
-  MonitorSmartphoneIcon,
-  ServerCogIcon,
-  ZapIcon,
-} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { appFeatures } from "@/constants";
 
-const features = [
-  {
-    name: "Store your PDF Documents",
-    description: "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
-    icon: GlobeIcon,
-  },
-  {
-    name: "Blazing Fast Responses",
-    description: "Experience lightning-fast answers to your queries, ensuring you get the information you need instantly.",
-    icon: ZapIcon,
-  },
-  {
-    name: "Chat Memorization",
-    description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
-    icon: BrainCogIcon,
-  },
-  {
-    name: "Interactive PDF Viewer",
-    description: "Engage with your PDFs like never before using our intuitive and interactive viewer.",
-    icon: EyeIcon,
-  },
-  {
-    name: "Cloud Backup",
-    description: "Rest assured knowing your documents are safely backed up on the cloud, protected from loss or damage.",
-    icon: ServerCogIcon,
-  },
-  {
-    name: "Responsive Across Devices",
-    description: "Access and chat with your PDFs seamlessly on any device, whether it's your desktop, yablet, or smartphone.",
-    icon: MonitorSmartphoneIcon,
-  },
-];
+const features = appFeatures;
 
 export default function Home() {
   return (
